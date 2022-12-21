7bad3c6ebf11d066006d74221263e2f0985f05b0
Jensen <jensenojs@qq.com>
2022-12-22 02:31:15 +0800

    Improve error reporting information

--- main.c
+++ main.c
@@ -5,14 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-static void error(const char *fmt, ...) {
-  va_list va;
-  va_start(va, fmt);         // get all arguments after fmt
-  vfprintf(stderr, fmt, va); // printf the arguments with va_list types
-  fprintf(stderr, "\n");
-  va_end(va);
-  exit(1);
-}
+static char *currentInput;
 
 typedef enum {
   TK_OPERATOR,
@@ -25,41 +18,81 @@ typedef struct Token {
   struct Token *next;
   int val;
   int len;
-  const char *idx;
+  char *idx;
 } Token;
 
-static Token *newToken(TokenKind kind, const char *start, const char *end) {
-  // to simplify, not considering memory recovery
-  Token *tok = calloc(1, sizeof(Token));
+static void error(const char *fmt, ...) {
+  va_list va;
+  va_start(va, fmt);         // get all arguments after fmt
+  vfprintf(stderr, fmt, va); // printf the arguments with va_list types
+  fprintf(stderr, "\n");
+  va_end(va);
+  exit(1);
+}
+
+// printf where error occurred
+static void _errorAt(char *idx, char *fmt, va_list va) {
+  // 0. print current input
+  fprintf(stderr, "%s\n", currentInput);
+
+  // 1. calculate the index where error occurred
+  int errorIdx = idx - currentInput;
+
+  // 2. print
+  fprintf(stderr, "%*s", errorIdx, ""); // filled spaces, similar to "%5d"
+  fprintf(stderr, "^ ");                // point out where error occurred
+  vfprintf(stderr, fmt, va);            // print specific error message
+  fprintf(stderr, "\n");
+
+  va_end(va);
+}
+
+// char parsing error
+static void errorAt(char *idx, char *fmt, ...) {
+  va_list va;
+  va_start(va, fmt);
+  _errorAt(idx, fmt, va);
+  exit(1);
+}
+
+// tok parsing error
+static void errorTok(Token *tok, char *fmt, ...) {
+  va_list va;
+  va_start(va, fmt);
+  _errorAt(tok->idx, fmt, va);
+  exit(1);
+}
+
+static Token *newToken(TokenKind kind, char *start, char *end) {
+  // to simplify, not considering memory recovery recycle
+  Token *tok = (Token *)calloc(1, sizeof(Token));
   tok->kind = kind;
   tok->idx = start;
   tok->len = end - start;
   return tok;
 }
 
-static bool tokenCompare(Token const *tok, const char *str) {
-  return memcmp(tok->idx, str, tok->len) == 0 && str[tok->len] == '\0';
+// verify that a token is expected
+static bool tokenCompare(Token const *tok, const char *expected) {
+  return memcmp(tok->idx, expected, tok->len) == 0 &&
+         expected[tok->len] == '\0';
 }
 
-static Token *tokenSkip(Token *tok, const char *str) {
-  if (!tokenCompare(tok, str)) {
+// if the token is expected, then skip it
+static Token *tokenSkip(Token *tok, const char *expected) {
+  if (!tokenCompare(tok, expected)) {
     char dest[tok->len];
     memcpy(dest, tok->idx, tok->len);
     dest[tok->len] = '\0';
-    error("expected str is %s, but got %s\n", str, dest);
+    errorTok(tok, "expected str is %s, but got %s\n", expected, dest);
   }
   return tok->next;
 }
 
-static int getNumber(Token *tok) {
-  if (tok->kind != TK_NUM)
-    error("expected number");
-  return tok->val;
-}
-
-static Token *tokenize(char *p) {
+static Token *tokenize() {
   Token head = {}; // empty head
   Token *cur = &head;
+  char *p = currentInput;
 
   while (*p) {
     // Skip all blank characters
@@ -84,11 +117,18 @@ static Token *tokenize(char *p) {
       continue;
     }
 
-    error("invalid token: %c", *p);
+    errorAt(p, "invalid token");
   }
   // add eof to represents the end
   cur->next = newToken(TK_EOF, p, p);
   return head.next;
+}
+
+// get the value of TK_NUM
+static int getNumber(Token *tok) {
+  if (tok->kind != TK_NUM)
+    errorTok(tok, "expected number");
+  return tok->val;
 }
 
 int main(int argc, char **argv) {
@@ -96,7 +136,8 @@ int main(int argc, char **argv) {
     error("%s: Invalid number of arguments %d", argv[0], argc);
   }
 
-  Token *tok = tokenize(argv[1]);
+  currentInput = argv[1];
+  Token *tok = tokenize();
 
   printf("  .global main\n");
   printf("main:\n");
