a0ded69e7f0ce38f47024398e7bf34478faac351
Jensen <jensenojs@qq.com>
2022-12-23 12:03:00 +0800

    support *, /, ()
     * lexical analysis
     * syntax analysis
     * Semantic analysis and code generation

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <ctype.h>
 #include <stdarg.h>
 #include <stdbool.h>
@@ -6,20 +7,7 @@
 #include <string.h>
 
 static char *currentInput;
-
-typedef enum {
-  TK_OPERATOR,
-  TK_NUM,
-  TK_EOF,
-} TokenKind;
-
-typedef struct Token {
-  TokenKind kind;
-  struct Token *next;
-  int val;
-  int len;
-  char *idx;
-} Token;
+static int Depth;
 
 static void error(const char *fmt, ...) {
   va_list va;
@@ -55,21 +43,40 @@ static void errorAt(char *idx, char *fmt, ...) {
   exit(1);
 }
 
+/*
+ * Lexical analysis
+ */
+
+typedef enum {
+  TK_INVALID = 0,
+  TK_PUNCT,
+  TK_NUM,
+  TK_EOF,
+} TokenType;
+
+typedef struct Token {
+  TokenType type;
+  struct Token *next;
+  char *idx; // indicates the starting position of this  in the input stream
+  int val;   // use if TokenType is TK_NUM
+  int len;
+} Token;
+
+static Token *newToken(TokenType type, char *start, char *end) {
+  // to simplify, not considering memory recovery recycle
+  Token *tok = (Token *)calloc(1, sizeof(Token));
+  tok->type = type;
+  tok->idx = start;
+  tok->len = end - start;
+  return tok;
+}
+
 // tok parsing error
 static void errorTok(Token *tok, char *fmt, ...) {
   va_list va;
   va_start(va, fmt);
   _errorAt(tok->idx, fmt, va);
   exit(1);
-}
-
-static Token *newToken(TokenKind kind, char *start, char *end) {
-  // to simplify, not considering memory recovery recycle
-  Token *tok = (Token *)calloc(1, sizeof(Token));
-  tok->kind = kind;
-  tok->idx = start;
-  tok->len = end - start;
-  return tok;
 }
 
 // verify that a token is expected
@@ -89,6 +96,7 @@ static Token *tokenSkip(Token *tok, const char *expected) {
   return tok->next;
 }
 
+// lexical analysis
 static Token *tokenize() {
   Token head = {}; // empty head
   Token *cur = &head;
@@ -110,8 +118,8 @@ static Token *tokenize() {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
-      cur->next = newToken(TK_OPERATOR, p, p + 1);
+    if (ispunct(*p)) {
+      cur->next = newToken(TK_PUNCT, p, p + 1);
       cur = cur->next;
       ++p;
       continue;
@@ -126,9 +134,180 @@ static Token *tokenize() {
 
 // get the value of TK_NUM
 static int getNumber(Token *tok) {
-  if (tok->kind != TK_NUM)
+  if (tok->type != TK_NUM)
     errorTok(tok, "expected number");
   return tok->val;
+}
+
+/*
+ * Generate AST (Abstract Syntax Tree), syntax analysis
+ * here are some rules for how to generate AST tree to support basic arithmetic
+ * like (), *, /
+ */
+
+typedef enum {
+  ND_INVALID = 0,
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // int
+} NodeType;
+
+typedef struct Node {
+  NodeType type;
+  struct Node *left;
+  struct Node *right;
+  int val;
+} Node;
+
+static Node *newNode(NodeType type) {
+  Node *d = calloc(1, sizeof(Node));
+  d->type = type;
+  return d;
+}
+
+static Node *newNum(int val) {
+  Node *d = newNode(ND_NUM);
+  d->val = val;
+  return d;
+}
+
+static Node *newBinary(NodeType type, Node *left, Node *right) {
+  Node *d = newNode(type);
+  d->left = left;
+  d->right = right;
+  return d;
+}
+
+// expr = mul ( "+" mul | "-" mul ) *
+static Node *expr(Token **rest, Token *tok);
+
+// mul = primary ("*" primary | "/" primary) *
+static Node *mul(Token **rest, Token *tok);
+
+// primary = "(" expr ")" | num
+static Node *primary(Token **rest, Token *tok);
+
+static Node *expr(Token **rest, Token *tok) {
+  Node *d = mul(&tok, tok);
+
+  // ( "+" mul | "-" mul ) *
+  while (true) {
+    if (tokenCompare(tok, "+")) {
+      d = newBinary(ND_ADD, d, mul(&tok, tok->next));
+      continue;
+    }
+
+    if (tokenCompare(tok, "-")) {
+      d = newBinary(ND_SUB, d, mul(&tok, tok->next));
+      continue;
+    }
+
+    break;
+  }
+
+  *rest = tok;
+  return d;
+}
+
+static Node *mul(Token **rest, Token *tok) {
+  // primary
+  Node *d = primary(&tok, tok);
+
+  // ("*" primary | "/" primary) *
+  while (true) {
+    if (tokenCompare(tok, "*")) {
+      d = newBinary(ND_MUL, d, primary(&tok, tok->next));
+      continue;
+    }
+
+    if (tokenCompare(tok, "/")) {
+      d = newBinary(ND_DIV, d, primary(&tok, tok->next));
+      continue;
+    }
+
+    break;
+  }
+
+  *rest = tok;
+  return d;
+}
+
+static Node *primary(Token **rest, Token *tok) {
+  // "(" expr ")"
+  if (tokenCompare(tok, "(")) {
+    Node *d = expr(&tok, tok->next);
+    *rest = tokenSkip(tok, ")");
+    return d;
+  }
+
+  // num
+  if (tok->type == TK_NUM) {
+    Node *d = newNum(tok->val);
+    *rest = tok->next;
+    return d;
+  }
+
+  errorTok(tok, "invalid expression");
+  return NULL;
+}
+
+/*
+ *  Semantic analysis and code generation
+ */
+static int StackDepth;
+
+// push the value of a0 onto the stack
+static void push() {
+  //  sp is the stack pointer, the stack grows downwards, under 64
+  //  bits, 8 bytes is a unit, so sp-8
+  printf("  addi sp, sp, -8\n");
+  //  press the value of a0 onto the stack
+  printf("  sd a0, 0(sp)\n");
+  StackDepth++;
+}
+
+// pop the value of the address pointed to by sp into the register
+static void pop(char *reg) {
+  printf("  ld %s, 0(sp)\n", reg);
+  printf("  addi sp, sp, 8\n");
+  StackDepth--;
+}
+
+// traversing the AST tree to generate assembly code
+static void genExpr(Node *node) {
+  // load data to a0 register
+  if (node->type == ND_NUM) {
+    printf("  li a0, %d\n", node->val);
+    return;
+  }
+
+  // consider the priority, recurse to the right node first
+  genExpr(node->right);
+  push();
+  genExpr(node->left);
+  pop("a1");
+
+  // generate what each binary tree node does in assembly code
+  switch (node->type) {
+  case ND_ADD:
+    printf("  add a0, a0, a1\n");
+    return;
+  case ND_SUB:
+    printf("  sub a0, a0, a1\n");
+    return;
+  case ND_MUL:
+    printf("  mul a0, a0, a1\n");
+    return;
+  case ND_DIV:
+    printf("  div a0, a0, a1\n");
+    return;
+  default:
+    break;
+  }
+
+  error("invalid expression");
 }
 
 int main(int argc, char **argv) {
@@ -136,33 +315,22 @@ int main(int argc, char **argv) {
     error("%s: Invalid number of arguments %d", argv[0], argc);
   }
 
+  // parse argv[1] to generate a stream of tokens
   currentInput = argv[1];
   Token *tok = tokenize();
+
+  // parse the stream of tokens
+  Node *node = expr(&tok, tok);
 
   printf("  .global main\n");
   printf("main:\n");
 
-  // num (op num) (op num)
-  printf("  li a0, %ld\n", (long)getNumber(tok));
-  tok = tok->next;
-
-  // resolve (op num)
-  while (tok->kind != TK_EOF) {
-    if (tokenCompare(tok, "+")) {
-      tok = tok->next;
-      printf("  addi a0, a0, %ld\n", (long)getNumber(tok));
-      tok = tok->next;
-      continue;
-    }
-
-    tok = tokenSkip(tok, "-");
-    printf("  addi a0, a0, -%ld\n", (long)getNumber(tok));
-    tok = tok->next;
-    continue;
-  }
+  genExpr(node);
 
   // ret is alias for "jalr x0, x1, 0"
   printf("  ret\n");
+
+  assert(Depth == 0);
 
   return 0;
 }
